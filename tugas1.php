a69bd28741f76e729bafa63ab33b6c6b40c2bc08
ddn05 <dadannazril@gmail.com>
2021-04-01 16:41:42 +0700

    tugas 1

---
+++ tugas1.php
@@ -0,0 +1,24 @@
+<?php
+    $h1  = 1000;
+    $h3  = 3000;
+    $h4  = 4000;
+    $h5  = 5000;
+    $h13 = 13000;
+    $h15 = 15000;
+
+    echo "<h1>Daftar Menu</h1>";
+    echo "<ul>
+
+        <li>Ayam Goreng  = Rp. $h13</li>
+        <li>Ayam Bakar   = Rp. $h15</li>
+        <li>Ayam Sayur   = Rp. $h13</li>
+        <li>Tempe Goreng = Rp. $h1</li>
+        <li>Tahu Goreng  = Rp. $h1</li>
+        <li>Soto         = Rp. $h3</li>
+        <li>Nasi Putih   = Rp. $h5</li>
+        <li>Es Teh       = Rp. $h4</li>
+        <li>Es Tebu      = Rp. $h5</li>
+
+    </ul>";
+
+?>
---
+++ tugas1_2.php
@@ -0,0 +1,43 @@
+<?php
+    $h1  = 1000;
+    $h5  = 5000;
+    $h15 = 15000;
+    $h20 = 20000;
+
+    echo "<h1>Daftar Menu</h1>";
+    echo "<table border='1'>
+
+        <tr>
+            <th>Nomor</th>
+            <th>Nama Menu</th>
+            <th>Harga</th>
+        <tr>
+        <tr>
+            <td>1</td>
+            <td>Cireng</td>
+            <td>Rp. $h1</td>
+        <tr>
+        <tr>
+            <td>2</td>
+            <td>Es Duren</td>
+            <td>Rp. $h15</td>
+        <tr>
+        <tr>
+            <td>3</td>
+            <td>Es Milo</td>
+            <td>Rp. $h20</td>
+        <tr>
+        <tr>
+            <td>4</td>
+            <td>Es Teh</td>
+            <td>Rp. $h5</td>
+        <tr>
+        <tr>
+            <td>5</td>
+            <td>Piscok</td>
+            <td>Rp. $h1</td>
+        <tr>
+
+    </table>";
+
+?>
